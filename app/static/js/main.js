db5a6aa3802a1bfc5f4921a48734b4bca3cbb4a3
Haliont <zemliakov.konstantin@gmai.com>
2017-05-26 21:51:20 +0600

    Страница поставщикам, открытие корзины с товарами

---
+++ app/static/js/main.js
@@ -0,0 +1,7 @@
+$(".js-show-basket").click(function() {
+  $('.basket__product-wrap').addClass('_active');
+})
+
+$(".js-hide-basket").click(function() {
+  $('.basket__product-wrap').removeClass('_active');
+})
